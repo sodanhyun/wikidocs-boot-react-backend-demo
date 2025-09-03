2b4923ff7a99029ba400575f31eed73ad3199286
박단현 <166380533+sodanhyun@users.noreply.github.com>
2025-09-03 17:42:54 +0900

    fix

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:21-jdk-slim
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java", "-Dspring.profiles.active=prod", "-jar", "/app.jar"]
+EXPOSE 8080
--- src/main/java/com/packt/cardatabase/domain/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.packt.cardatabase.domain.repository;
-
-import com.packt.cardatabase.domain.Car;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarRepository extends CrudRepository<Car, Long> {
-}
